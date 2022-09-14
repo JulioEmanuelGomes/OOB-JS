2a903e619dac955d3b83b2e6968a3bbc5c4d7d82
JulioEmanuelGomes <julioemanuel172@gmail.com>
2022-09-14 12:37:05 -0300

    Finalizei

---
+++ Imotor.js
@@ -0,0 +1,20 @@
+class Imotor  {
+#nitro = 'STUTUTUTU (Turbina)';
+
+
+    constructor(){
+
+    }
+
+    acelerarGOL(){
+        return "Vrummm....(Gol)";
+    } 
+    acelerarBMW(){
+        return `BMW: VRUMMMMMMMMM!!!....${this.#nitro}` ;
+
+    } 
+    acelerarMER(){
+        return "VRUMMMMM!!...(MERCEDES)";
+    }
+
+}
